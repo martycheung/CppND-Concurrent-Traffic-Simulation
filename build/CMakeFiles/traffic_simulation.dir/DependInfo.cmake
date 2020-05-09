1a1c354bad1ace7a47554868dc7feac0352c586a
martycheung <martin.cheung13@gmail.com>
2020-05-09 15:33:39 +0200

    Import Concurrent Traffic Simulation Project

---
+++ build/CMakeFiles/traffic_simulation.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/src/Graphics.cpp" "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Graphics.cpp.o"
+  "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/src/Intersection.cpp" "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Intersection.cpp.o"
+  "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/src/Street.cpp" "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Street.cpp.o"
+  "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/src/TrafficLight.cpp" "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficLight.cpp.o"
+  "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/src/TrafficObject.cpp" "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficObject.cpp.o"
+  "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/src/TrafficSimulator-Final.cpp" "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/TrafficSimulator-Final.cpp.o"
+  "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/src/Vehicle.cpp" "/Users/martincheung/Desktop/Learning Resources/C++ Nanodegree/Udacity-Cpp-Nanodegree/Project4-Concurrent-Traffic-Simulation/build/CMakeFiles/traffic_simulation.dir/src/Vehicle.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/martincheung/Documents/CMake/build_opencv"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/core/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/flann/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/imgproc/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/ml/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/photo/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/dnn/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/features2d/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/gapi/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/imgcodecs/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/videoio/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/calib3d/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/highgui/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/objdetect/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/stitching/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/ts/include"
+  "/Users/martincheung/Documents/CMake/opencv-4.3.0/modules/video/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
